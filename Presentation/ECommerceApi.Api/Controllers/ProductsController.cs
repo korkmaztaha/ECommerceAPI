8cec4172e7382cd3485867bb824980a4a63bebdf
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-02-23 17:38:47 +0300

    Products controller created

---
+++ Presentation/ECommerceApi.Api/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ECommerceApi.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new(){Id=Guid.NewGuid(), Name="p4",Price=11, CreatedDate=DateTime.UtcNow, Stock=22},
+                new(){Id=Guid.NewGuid(), Name="p5",Price=21, CreatedDate=DateTime.UtcNow, Stock=32},
+                new(){Id=Guid.NewGuid(), Name="p6",Price=31, CreatedDate=DateTime.UtcNow, Stock=42},
+            });
+           await _productWriteRepository.SaveAsync();
+
+
+        }
+    }
+}
