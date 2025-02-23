060c22a1d43e1aa22e22107affc88fd9e5e78e00
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-02-23 13:54:09 +0300

    some changes

--- Core/ECommerce.Application/Repositories/IWriteRepository.cs
+++ Core/ECommerce.Application/Repositories/IWriteRepository.cs
@@ -7,13 +7,15 @@ using System.Threading.Tasks;
 
 namespace ECommerceApi.Application.Repositories
 {
-    public interface IWriteRepository<T>:IRepository<T> where T : BaseEntity
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
     {
         Task<bool> AddAsync(T model);
-        Task<bool> AddAsync(List<T> model);
-        Task<bool> UpdateAsync(T model); 
-        Task<bool> Remove(T model);
-        Task<bool> Remove(string id);
+        Task<bool> AddRangeAsync(List<T> datas);
+        bool Update(T model);
+        bool Remove(T model);
+        Task<bool> RemoveAsync(string id);
+        bool RemoveRange(List<T> datas);
+        Task<int> SaveAsync();
 
     }
 }
