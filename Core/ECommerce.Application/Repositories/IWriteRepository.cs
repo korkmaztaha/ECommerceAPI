b899d7c393c355cdd007bd0b317405a4aaa337fb
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-02-23 12:31:23 +0300

    IWriteRepository development

--- Core/ECommerce.Application/Repositories/IWriteRepository.cs
+++ Core/ECommerce.Application/Repositories/IWriteRepository.cs
@@ -8,5 +8,11 @@ namespace ECommerceApi.Application.Repositories
 {
     public interface IWriteRepository<T>:IRepository<T> where T : class
     {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddAsync(List<T> model);
+        Task<bool> UpdateAsync(T model); 
+        Task<bool> Remove(T model);
+        Task<bool> Remove(string id);
+
     }
 }
