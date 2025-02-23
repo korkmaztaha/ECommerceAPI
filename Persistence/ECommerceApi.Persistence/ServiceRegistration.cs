5c25a72efd23059352143bacf08c00debb7aa58a
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-02-23 14:55:22 +0300

    services added to IoC Container

--- Persistence/ECommerceApi.Persistence/ServiceRegistration.cs
+++ Persistence/ECommerceApi.Persistence/ServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using ECommerceApi.Persistence.Contexts;
+﻿using ECommerceApi.Application.Repositories;
+using ECommerceApi.Persistence.Contexts;
+using ECommerceApi.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,8 +15,14 @@ namespace ECommerceApi.Persistence
     {
         public static void AddPersitenceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ECommerceAPIDbContext>(options=> options.UseNpgsql(Configuration.ConnectionString));
-           
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
+            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+ 
         }
 
     }
