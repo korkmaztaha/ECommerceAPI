da8251cc4aa8892d73c4ad7454b36c4a7d932d96
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-03-02 08:17:32 +0300

    override prop

---
+++ Core/ECommerce.Domain/Entities/File.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceApi.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Domain.Entities
+{
+    public class File:BaseEntity
+    {
+        public int MyProperty { get; set; }
+
+        [NotMapped]
+        public override DateTime UpdatedDate { get => base.UpdatedDate; set => base.UpdatedDate = value; }
+    }
+}
