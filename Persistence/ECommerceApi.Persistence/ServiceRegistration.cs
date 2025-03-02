717d94151e2cb50acb421fb571ed90d6a0320570
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-03-02 18:24:09 +0300

    ioc added

--- Persistence/ECommerceApi.Persistence/ServiceRegistration.cs
+++ Persistence/ECommerceApi.Persistence/ServiceRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using ECommerceApi.Application.Repositories;
 using ECommerceApi.Persistence.Contexts;
 using ECommerceApi.Persistence.Repositories;
+using ECommerceApi.Persistence.Repositories.File;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -22,7 +23,13 @@ namespace ECommerceApi.Persistence
             services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
             services.AddScoped<IProductReadRepository, ProductReadRepository>();
             services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
- 
+            services.AddScoped<IFileReadRepository, FileReadRepository>();
+            services.AddScoped<IFileWriteRepository, FileWriteRepository>();
+            services.AddScoped<IProductImageFileReadRepository, ProductImageFileReadRepository>();
+            services.AddScoped<IProductImageFileWriteRepository, ProductImageFileWriteRepository>();
+            services.AddScoped<IInvoiceFileReadRepository, InvoiceFileReadRepository>();
+            services.AddScoped<IInvoiceFileWriteRepository, InvoiceFileWriteRepository>();
+
         }
 
     }
