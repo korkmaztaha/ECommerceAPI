37249e91e1608532a59185c596d5e8f367b96dfc
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-03-15 07:16:16 +0300

    Basket repositories created

---
+++ Persistence/ECommerceApi.Persistence/Repositories/Basket/BasketReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceApi.Application.Repositories;
+using ECommerceApi.Domain.Entities;
+using ECommerceApi.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Persistence.Repositories
+{
+    public class BasketReadRepository : ReadRepository<Basket>, IBasketReadRepository
+    {
+        public BasketReadRepository(ECommerceAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
