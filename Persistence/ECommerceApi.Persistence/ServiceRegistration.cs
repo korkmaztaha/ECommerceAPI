0d48ddb0eacde3d279c3e8e501b95315882909f6
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-03-15 07:18:33 +0300

    basket service registration

--- Persistence/ECommerceApi.Persistence/ServiceRegistration.cs
+++ Persistence/ECommerceApi.Persistence/ServiceRegistration.cs
@@ -44,6 +44,10 @@ namespace ECommerceApi.Persistence
             services.AddScoped<IProductImageFileWriteRepository, ProductImageFileWriteRepository>();
             services.AddScoped<IInvoiceFileReadRepository, InvoiceFileReadRepository>();
             services.AddScoped<IInvoiceFileWriteRepository, InvoiceFileWriteRepository>();
+            services.AddScoped<IBasketItemReadRepository, BasketItemReadRepository>();
+            services.AddScoped<IBasketItemWriteRepository, BasketItemWriteRepository>();
+            services.AddScoped<IBasketReadRepository, BasketReadRepository>();
+            services.AddScoped<IBasketWriteRepository, BasketWriteRepository>();
 
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IAuthService, AuthService>();
