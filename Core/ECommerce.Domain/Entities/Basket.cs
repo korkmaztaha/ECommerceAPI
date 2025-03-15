6644373e0c2e398de1dc9db32a68afa146faa8f4
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-03-15 07:15:41 +0300

    Basket migration

---
+++ Core/ECommerce.Domain/Entities/Basket.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceApi.Domain.Entities.Common;
+using ECommerceApi.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Domain.Entities
+{
+    public class Basket : BaseEntity
+    {
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+        public ICollection<BasketItem> BasketItems { get; set; }
+
+    }
+}
--- Core/ECommerce.Domain/Entities/Identity/AppUser.cs
+++ Core/ECommerce.Domain/Entities/Identity/AppUser.cs
@@ -13,5 +13,7 @@ namespace ECommerceApi.Domain.Entities.Identity
         public string NameSurname { get; set; }
         public string? RefreshToken { get; set; }
         public  DateTime? RefreshTokenEndDate { get; set; }
+        public ICollection<Basket> Baskets { get; set; }
     }
+    
 }
