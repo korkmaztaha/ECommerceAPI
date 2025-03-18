d900d85cd1373e7bdf3873287f4ca8a5d16a21f7
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-03-18 07:48:16 +0300

    orders controller created

--- Presentation/ECommerceApi.Api/Controllers/OrdersController.cs
+++ Presentation/ECommerceApi.Api/Controllers/OrdersController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerceApi.Application.Features.Commands.Order.CreateOrder;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceApi.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = "Admin")]
     public class OrdersController : ControllerBase
     {
+        readonly IMediator _mediator;
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+       
+        public async Task<ActionResult> CreateOrder(CreateOrderCommandRequest createOrderCommandRequest)
+        {
+            CreateOrderCommandResponse response = await _mediator.Send(createOrderCommandRequest);
+            return Ok(response);
+        }
     }
 }
