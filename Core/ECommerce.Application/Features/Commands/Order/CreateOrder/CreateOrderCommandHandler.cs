1ca6d242673ef95c0e2ae5f3e0bdda3879d0cc55
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-03-18 07:47:26 +0300

    order cqrs implemented

---
+++ Core/ECommerce.Application/Features/Commands/Order/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceApi.Application.Abstractions.Hubs;
+using ECommerceApi.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Features.Commands.Order.CreateOrder
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommandRequest, CreateOrderCommandResponse>
+    {
+        readonly IOrderService _orderService;
+        readonly IBasketService _basketService;
+        readonly IOrderHubService _orderHubService;
+
+        public CreateOrderCommandHandler(IOrderHubService orderHubService, IBasketService basketService, IOrderService orderService)
+        {
+            _orderHubService = orderHubService;
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        public async Task<CreateOrderCommandResponse> Handle(CreateOrderCommandRequest request, CancellationToken cancellationToken)
+        {
+            await _orderService.CreateOrderAsync(new()
+            {
+                Address = request.Address,
+                Description = request.Description,
+                BasketId = _basketService.GetUserActiveBasket?.Id.ToString()
+            });
+
+            await _orderHubService.OrderAddedMessageAsync("Yeni bir sipariş geldi");
+
+            return new();
+        }
+    }
+}
