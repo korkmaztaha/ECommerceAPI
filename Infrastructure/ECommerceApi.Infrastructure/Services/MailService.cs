73df41b9a9c0d3de59311cd1763881134477181a
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-04-07 18:39:09 +0300

    reset password mail func. created

--- Infrastructure/ECommerceApi.Infrastructure/Services/MailService.cs
+++ Infrastructure/ECommerceApi.Infrastructure/Services/MailService.cs
@@ -19,13 +19,13 @@ namespace ECommerceApi.Infrastructure.Services
             _configuration = configuration;
         }
 
-        public async Task SendMessageAsync(string to, string subject, string body, bool isBodyHtml = true)
+        public async Task SendMailAsync(string to, string subject, string body, bool isBodyHtml = true)
         {
-            await SendMessageAsync(new[] { to }, subject, body, isBodyHtml);
+            await SendMailAsync(new[] { to }, subject, body, isBodyHtml);
 
         }
 
-        public async Task SendMessageAsync(string[] toS, string subject, string body, bool isBodyHtml = true)
+        public async Task SendMailAsync(string[] toS, string subject, string body, bool isBodyHtml = true)
         {
             MailMessage mail = new();
             mail.IsBodyHtml = isBodyHtml;
@@ -60,7 +60,21 @@ namespace ECommerceApi.Infrastructure.Services
             }
         }
 
+        public async Task SendPasswordResetMailAsync(string to, string userId, string resetToken)
+        {
 
+            StringBuilder mail = new();
+            mail.AppendLine("Merhaba<br>Eğer yeni şifre talebinde bulunduysanız aşağıdaki linkten şifrenizi yenileyebilirsiniz.<br><strong><a target=\"_blank\" href=\"");
+         
+            mail.AppendLine("/update-password/");
+            mail.AppendLine(userId);
+            mail.AppendLine("/");
+            mail.AppendLine(resetToken);
+            mail.AppendLine("\">Yeni şifre talebi için tıklayınız...</a></strong><br><br><span style=\"font-size:12px;\">NOT : Eğer ki bu talep tarafınızca gerçekleştirilmemişse lütfen bu maili ciddiye almayınız.</span>");
+
+            await SendMailAsync(to, "Şifre Yenileme Talebi", mail.ToString());
+
+        }
     }
 }
 
