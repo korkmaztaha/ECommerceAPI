aba4a5a96f4105c412d6c32de70a2d931b160dfd
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-04-14 19:21:43 +0300

    role service CQRS items creted

---
+++ Core/ECommerce.Application/Features/Queries/Role/GetRoles/GetRolesQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using ECommerceApi.Application.Abstractions.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Features.Queries.Role.GetRoles
+{
+
+    public class GetRolesQueryHandler : IRequestHandler<GetRolesQueryRequest, GetRolesQueryResponse>
+    {
+        readonly IRoleService _roleService;
+
+        public GetRolesQueryHandler(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public async Task<GetRolesQueryResponse> Handle(GetRolesQueryRequest request, CancellationToken cancellationToken)
+        {
+            var datas = _roleService.GetAllRoles();
+            return new()
+            {
+                Datas = datas,
+                
+            };
+        }
+    }
+}
