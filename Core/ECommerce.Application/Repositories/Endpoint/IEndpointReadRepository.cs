646cf45e2ae3bd530abb445b5ec6096486a00ecb
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-04-16 20:19:30 +0300

    repositories creted

---
+++ Core/ECommerce.Application/Repositories/Endpoint/IEndpointReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Repositories
+{
+    public interface IEndpointReadRepository : IReadRepository<Endpoint>
+    {
+
+    }
+}
---
+++ Core/ECommerce.Application/Repositories/Menu/IMenuWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceApi.Application.DTOs.Configuration;
+using ECommerceApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Repositories
+{
+    public interface IMenuWriteRepository : IWriteRepository<Menu>
+    {
+    }
+}
