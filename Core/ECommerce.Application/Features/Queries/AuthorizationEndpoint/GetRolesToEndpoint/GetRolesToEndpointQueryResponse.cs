a4bfe6508341234d6787a3a34f6f7d74ca9756e0
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-04-16 20:19:04 +0300

    CQRS items created

---
+++ Core/ECommerce.Application/Features/Queries/AuthorizationEndpoint/GetRolesToEndpoint/GetRolesToEndpointQueryResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Features.Queries.AuthorizationEndpoint.GetRolesToEndpoint
+{
+    public class GetRolesToEndpointQueryResponse
+    {
+        public object Roles { get; set; }
+    }
+}
