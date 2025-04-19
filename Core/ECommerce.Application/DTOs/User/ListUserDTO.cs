68da587d5fa34c82a90bdcfe9043e39db1008aef
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-04-19 16:45:07 +0300

    ListUser Dto created

---
+++ Core/ECommerce.Application/DTOs/User/ListUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.DTOs.User
+{
+    public class ListUserDTO
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string NameSurname { get; set; }
+        public string UserName { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+
+    }
+}
