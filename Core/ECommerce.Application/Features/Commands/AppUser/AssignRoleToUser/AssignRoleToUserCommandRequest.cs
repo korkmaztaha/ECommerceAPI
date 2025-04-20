437ef73b16266aad766711571f3c41831ae7357d
Taha Korkmaz <taha.korkmaz@patikaglobal.com>
2025-04-20 08:25:14 +0300

    CQRS items created

---
+++ Core/ECommerce.Application/Features/Commands/AppUser/AssignRoleToUser/AssignRoleToUserCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Application.Features.Commands.AppUser.AssignRoleToUser
+{
+    public class AssignRoleToUserCommandRequest : IRequest<AssignRoleToUserCommandResponse>
+    {
+        public string UserId { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
